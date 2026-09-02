d0db0fb6b1956aa77c4bbc32cf80d8aa18734284
wngyrnr <wngyrmr@gmail.com>
2026-09-02 11:38:31 +0900

    BoardList get요청 파이프라인 작성

--- src/main/java/com/example/tester/BoardList.java
+++ src/main/java/com/example/tester/BoardList.java
@@ -2,6 +2,11 @@ package com.example.tester;
 
 public class BoardList {
     /*
+    게시판 조회
 
-     */
+    url 요청 받기 - GET /api/board 로 요청이 들어옴
+    db에 요청보내기 - 비지니스 로직 거쳐서 매퍼를 호출함/mapper실행
+    가져온 정보 담기 - mapper 쿼리로 각 low를 가져와 매핑함
+    담긴 정보 return으로 넘겨줌 - 로직에서 다시 요청받은 경로로 반환함
+    */
 }
