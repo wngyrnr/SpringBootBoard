8dad7c4db6c14475eef100ae995fd2bf26b0c7ec
wngyrnr <wngyrmr@gmail.com>
2026-09-06 23:47:54 +0900

    feat: 게시글 목록 조회 API 구현 (GET /board/list)
    - created_at 기준 정렬로 전체 목록 조회 성공

--- src/main/java/com/example/tester/boardselect/BoardSelectApi.java
+++ src/main/java/com/example/tester/boardselect/BoardSelectApi.java
@@ -1,25 +1,49 @@
-package com.example.tester.select;
+package com.example.tester.boardselect;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+
+
 @Controller
+@RestController
 @RequestMapping("/board")
-@ResponseBody
 public class BoardSelectApi {
 
-    public String boardSelect(){
-        return null;
+    @Autowired
+    private BoardListMapper boardlistmapper;
+
+
+    @GetMapping("/list")
+    public List<BoardListDto> boardSelect() {
+        List<BoardListDto> boardlist = boardlistmapper.findAll();
+        return boardlist;
     }
+}
+    @Getter
+    @AllArgsConstructor
+    class BoardListDto{
+        private String category;
+        private String title;
+        private String writer;
+        private LocalDateTime createAt;
+    }
+
+@Mapper
+interface BoardListMapper{
+    List<BoardListDto> findAll();
 }
 
 /*
@@ -37,26 +61,3 @@ public class BoardSelectApi {
 
 
  */
-
-
-    class boardListservice{
-        public List<BoardListDto> boardlist(){
-            boardListmapper.findAll(List<BoardListDto>);
-
-            return null;
-
-        }
-}
-
-    @Getter
-    class BoardListDto{
-        private String category;
-        private String title;
-        private String writer;
-        private LocalDateTime createAt;
-    }
-
-    @Mapper
-    interface BoardListMapper{
-        List<BoardListDto> findAll(List<BoardListDto> boardlist);
-    }
