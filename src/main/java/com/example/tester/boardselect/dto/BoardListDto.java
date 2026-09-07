578a2cd460c3cce18ee14602a553271d7d480666
wngyrnr <wngyrmr@gmail.com>
2026-09-08 00:51:32 +0900

    boardselect계층분리

---
+++ src/main/java/com/example/tester/boardselect/dto/BoardListDto.java
@@ -0,0 +1,15 @@
+package com.example.tester.boardselect.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class BoardListDto{
+    private String category;
+    private String title;
+    private String writer;
+    private LocalDateTime createAt;
+}
---
+++ src/main/java/com/example/tester/boardselect/service/BoardSelectService.java
@@ -0,0 +1,9 @@
+package com.example.tester.boardselect.service;
+
+import com.example.tester.boardselect.dto.BoardListDto;
+
+import java.util.List;
+
+public interface BoardSelectService {
+    List<BoardListDto> boardSelect();
+}
